8ed10c52851a83d7ee4f02d1d80953630eefec12
Amit Naipaul <amitnaipaul@gmail.com>
2021-01-21 02:40:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -45,8 +45,18 @@ def game_hash
       steals: 3,
       blocks: 8,
       slam_dunks: 5
+    },
+    {
+      player_name: "Jason Terry",
+      number: 31,
+      shoe: 15,
+      points: 19,
+      rebounds: 2,
+      assists: 2,
+      steals: 4,
+      blocks: 11,
+      slam_dunks: 1
+    }]
 
-    }
-
-  ]
+    
   }
